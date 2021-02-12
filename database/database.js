d88532401c8dea33f5f6b3ffbcb96a20a736aecb
Alex <alexsandro.sousa12366@gmail.com>
2021-02-12 18:44:44 -0300

    initial commit

---
+++ database/database.js
@@ -0,0 +1,9 @@
+const { Model } = require("sequelize")
+const Sequelize = require("sequelize")
+
+const connection = new Sequelize('guiapress', 'root', '',{
+    host: 'localhost',
+    dialect: 'mysql'
+})
+
+module.exports = connection;
